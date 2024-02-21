96685ae003545164402c1db11264881fb2bba143
opilane <opilane@HP-CZC2349HV1>
2024-02-21 11:43:49 +0200

    dsaq

--- TARpv23_JO_1/MyModul.py
+++ TARpv23_JO_1/MyModul.py
@@ -9,3 +9,42 @@
     """
     s=arv1+arv2+arv3
     return s
+
+def typeControl() -> any:
+    """ Funktsioon kontrollib, millist tüüpi andmed on
+        typeControl()
+    :rtype: any
+    """
+    int_1=input("Sisetage andmed")
+    try:
+        
+        return int(int_1)
+    except :
+        try:
+            return float(int_1)
+        except:
+            return str(int_1)
+
+def arithmetic(arv1:float,arv2:float,operatsion:str)->any:
+    """ 3 argumenti: esimesed 2 on numbrid, kolmas on operatsioon, mis tuleb teha nende kohal. 
+        arithmetic=(arv1,arv2,operatsion)
+
+        :param float arv1: Arv1 sisestab kasutaja
+        :param float arv2: Arv2 sisestab kasutaja
+        :param str operatsion: Operatsion sisestab kasutaja + / - / * / "/" /
+        :rtype: any
+
+    """
+    if operatsion == "+":
+        return arv1 + arv2
+    elif operatsion == "-":
+        return arv1 - arv2
+    elif operatsion == "*":
+        return arv1 * arv2
+    elif operatsion== "/":
+        if arv2 !=0:
+            return arv1 / arv2
+        else:
+            return "Nulli ei saa jagada"
+    else:
+        return "Tundmatu operatsioon"
---
+++ TARpv23_JO_1/def.py
@@ -0,0 +1,19 @@
+﻿from MyModul import * #Summa as s
+#1
+#b=int(input("Siseta arv2: "))
+#summa_3=Summa(3,b,int(input("Kolmas arv: ")))
+#summa_31=Summa(100,100)
+
+#print(summa_3)
+#print(summa_31)
+#2
+#value=input("Siseta number: ")
+#value_control=typeControl(value) 
+#print(value_control)
+#(1)
+while True:
+    arv1=int(input("Siseta arv number 1: "))
+    arv2=int(input("Siseta arv number 2: "))
+    operatsion=input("Millist funktsiooni soovite kasutada? + / - / * / / /")
+    result=arithmetic(arv1,arv2,operatsion)
+    print(result)
